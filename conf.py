9689546b2d88ef4e5e96c1404d8b71ce2b24c820
nyutal <nyutal@yahoo-inc.com>
2017-01-23 12:04:55 +0200

    add test

--- conf.py
+++ conf.py
@@ -1,9 +1,9 @@
 class Conf(object):
     train_file_name = 'HW2-files/train.labeled'
-    train_max_samples = 100
+    train_max_samples = 1000 # less than 1 to represent full samples
     train_niter = 10
     test_file_name = 'HW2-files/train.labeled'
-    test_max_samples = 10
+    test_max_samples = 10 # less than 1 to represent full samples
     comp_file_name = 'comp.words'
     parallel = True
     num_of_learners = 4
--- perceptron.py
+++ perceptron.py
@@ -11,6 +11,7 @@ class Perceptron(object):
     def train(self, corpus: Corpus, fv: FeatureVec, niter: int):
         weights = np.ones(fv.get_size())
         for i in range(niter):
+            mistakes = 0.
             print('perceptron iteration ', i, time.asctime())
             n_sen = 0
             for s in corpus.get_sentences():
@@ -24,24 +25,27 @@ class Perceptron(object):
                 y = s.get_graph()
                 y_f = fv.get_features_for_graph(s, y)
 
-                # if not 0 in y_mst:
-                #     print(y)
-                #     print(g)
-                #     raise AssertionError(y_mst)
 
                 if (self.compare_graphs(y, y_mst)):
                     # print('equal graphs')
                     # print(y)
                     # print(y_mst)
+                    if not 0 in y_mst:
+                        print(y)
+                        print(g)
+                        print(self.compare_graphs(y, y_mst))
+                        raise AssertionError(y_mst)
                     continue
 
+
                 weights = weights + y_f - y_mst_f
-                print(weights)
+                # print(weights)
+                mistakes += 1.0
 
                 n_sen += 1
                 if n_sen % 1000 == 0:
                     print('perceptron processed ', n_sen, 'sentences')
-
+            print('perceptron iteration ', i, 'finished with', str(mistakes/n_sen) + '% difference graphs mistakes')
         return weights
 
     def build_clique_graph(self, s, fv, weights):
@@ -54,6 +58,8 @@ class Perceptron(object):
 
 
     def compare_graphs(self, y, y_mst):
+        if len(y_mst.keys()) != len(y.keys()):
+            return False
         for h in y_mst:
             if h not in y:
                 return False
@@ -72,9 +78,11 @@ class Perceptron(object):
             y_mst = ed.mst(0, g)
             for w in s.words[1:]:
                 nwords += 1.0
-                if w.counter in g[w.head]:
+                if w.head in y_mst and w.counter in y_mst[w.head]:
                     ncorrect += 1.0
-                else:print('error: (h,m)=(',w.head, w.counter,'), predicted graph:', y_mst)
+                else:
+                    print('error: (h,m)=(', w.head, w.counter, '), predicted graph:', y_mst)
+        print('test: words=', str(nwords), ', correct words=', str(ncorrect), ', precision=', str(ncorrect/nwords*100) + '%')
 
 
 
